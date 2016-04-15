37b9739e90484b6a340ec918687edf7cace7043c
JasonRVCC <G00136378@W213-30.academic.raritanval.edu>
2016-04-15 12:44:12 -0400

    session 1.1

---
+++ SpaceBilliards/Assets/Tester.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tester : MonoBehaviour {
+
+	public GameObject ball;
+
+	public Vector3 velocity;
+
+	public float z = 10;
+	public float x = 0;
+
+	void Start()
+	{
+		velocity = ball.rigidbody.velocity;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			ball.rigidbody.velocity = new Vector3(x, 0, z);
+		}
+
+		velocity = ball.rigidbody.velocity;
+	}
+}
